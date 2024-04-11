194afff9f7a24419566cdda7b701cf0ca8b80994
Subhojeet Chowhdury <84248777+subhojeet-chowdhury@users.noreply.github.com>
2024-04-11 21:41:00 +0000

    completed app UI modifications

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,7 +28,7 @@ def main():
     uploaded_file = st.file_uploader("Upload CSV file containing transaction data:", type=["csv"])
 
     # Prediction button
-    if st.button("Start"):
+    if st.button("Start Prediction"):
         if transaction_input:
             # Process single transaction input and make prediction
             process_single_transaction(transaction_input)
@@ -42,135 +42,137 @@ def main():
 
 def process_single_transaction(transaction_data):
     
-    # Display the single transaction data in a beautiful table
-    st.write("Received transaction data:")
+    # with st.spinner("Loading data..."):
 
-    # Simulate data loading
-    simulate_data_loading("Loading data...", 4)
+    # Convert transaction data to DataFrame
+    values = transaction_data.split(",")
+    values = [float(value) if index != len(values)-1 else int(value.strip('"')) for index, value in enumerate(values)]
+    columns = ["Time"] + [f"V{i}" for i in range(1, 29)] + ["Amount", "Class"]
+    df = pd.DataFrame([values], columns=columns)
 
-    df_transaction = pd.DataFrame([transaction_data.split(",")], columns=["Time", "V1", "V2", "V3", "V4", "V5", "V6", "V7", "V8", "V9", "V10", "V11", "V12", "V13", "V14", "V15", "V16", "V17", "V18", "V19", "V20", "V21", "V22", "V23", "V24", "V25", "V26", "V27", "V28", "Amount", "IsFraud"])
-    st.table(df_transaction)
+    simulate_loading("Loading data...",3)
+    st.success("Received transaction data:")
+    st.table(df)
 
-    # Display different visualizations
-    st.write("Data loaded successfully for testing")
+    st.success("Data loaded successfully for testing")
 
-    simulate_data_loading("Presenting visualizations...", 3)
+    with st.spinner("Presenting visualizations..."):
+        #Input Data Visualizations
+        st.header("Input Data Visualizations")
+        st.subheader("Histogram of Transaction Amount")
+        st.pyplot(plot_histogram(df))
 
-    #Input Data Visualizations
-    st.subheader("Input Data Visualizations")
-    st.write("Histogram of Transaction Amount")
-    st.pyplot(plot_histogram(df_transaction))
+        st.subheader("Bar plot of Class Distribution")
+        st.pyplot(plot_class_distribution(df))
 
-    st.write("Bar plot of Class Distribution")
-    st.pyplot(plot_class_distribution(df_transaction))
-
-    #Start Predictions
-    start_prediction(df_transaction)
+    # start prediction
+    start_prediction(df, "single-transaction")
 
 def process_uploaded_file(uploaded_file):
     
-    # Read CSV file
-    df = pd.read_csv(uploaded_file)
+    with st.spinner("Loading data..."):
+        df = pd.read_csv(uploaded_file)
 
-    # Simulate data loading
-    simulate_data_loading("Loading data...", 4)
-
+    simulate_loading("Loading data...",3)
     # Display the CSV file data in a beautiful table
-    st.write("Received CSV file:")
+    st.success("Received CSV file:")
     st.table(df)
-
     # Display different visualizations
-    st.write("Data loaded successfully for testing")
+    st.success("Data loaded successfully for testing")
+    
+    with st.spinner("Presenting visualizations..."):
+        # Visualizations
+        st.header("Input Data Visualizations")
+        st.subheader("Histogram of Transaction Amount")
+        st.pyplot(plot_histogram(df))
 
-    simulate_data_loading("Presenting visualizations...", 3)
-
-    # Visualizations
-    st.subheader("Input Data Visualizations")
-    st.write("Histogram of Transaction Amount")
-    st.pyplot(plot_histogram(df))
-
-    st.write("Bar plot of Class Distribution")
-    st.pyplot(plot_class_distribution(df))
-
-    # start predictions
-    start_prediction(df)
-
-
-def simulate_data_loading(message, loadTime):
-    # Simulate data loading with a sleep timer
-    with st.spinner(message):
-        time.sleep(loadTime)  # Simulate loading time (5 seconds)
-
-def start_prediction(df):
-
-    # Prediction button
-    if st.button("Load model and start Prediction"):
-       
-        # Load the model using the cached function
-        model = load_trained_model("oversampled_neural_network_model.keras")
-
-        if model:
-            st.write("Model loaded successfully!")
-
-            ## Perform predictions and evaluation
-            st.header("Perform Predictions and Evaluation")
-            predict_and_evaluate(df, model)
-        
-        else:
-            st.warning("Please upload a trained model.")
+        st.subheader("Bar plot of Class Distribution")
+        st.pyplot(plot_class_distribution(df))
+    
+    # start prediction
+    start_prediction(df, "uploaded-file")
 
 
+def start_prediction(df, input_type):
+
+    # Loading saved model from directory
+    simulate_loading("Loading saved model...",3)
+    model_name = "oversampled_neural_network_model.keras"
+    model = load_trained_model(model_name)
+    st.success("Model loaded successfully")
+    st.write(model_name)
+
+    simulate_loading("Performing predictions...",3)
+    st.header("Predictions and Evaluations")
+
+    predict_and_evaluate(df,model,input_type);
+    st.balloons()
 
 # Load trained model
 @st.cache_resource
 def load_trained_model(model_file):
-    # Load the Keras model
     model = load_model(model_file)
     return model
 
-# Perform predictions and evaluation
-def predict_and_evaluate(df, model):
+# Perform predictions and evaluation for uploaded file
+def predict_and_evaluate(df, model, input_file):
     if df is not None and model is not None:
-        st.write("Data loaded successfully for testing")
 
         # Separate features and target variable
-        X_test = df.drop(columns=["Class"])
+        X_test = df.drop(columns=["Class", "Time"])
         y_test = df["Class"]
 
         # Perform predictions
         start_time = time.time()
-        y_pred = model.predict(X_test)
+        y_pred_proba = model.predict(X_test)
         end_time = time.time()
+        
+        # Convert probability predictions to binary classification results
+        y_pred = (y_pred_proba > 0.5).astype(int)
 
         # Calculate evaluation metrics
-        accuracy = accuracy_score(y_test, y_pred.round())
-        precision = precision_score(y_test, y_pred.round())
-        recall = recall_score(y_test, y_pred.round())
-        f1 = f1_score(y_test, y_pred.round())
+        accuracy = accuracy_score(y_test, y_pred)
+        precision = precision_score(y_test, y_pred)
+        recall = recall_score(y_test, y_pred)
+        f1 = f1_score(y_test, y_pred)
+
+        # Determine whether the prediction is true or false for each transaction
+        prediction_result = ["True Positive" if pred == 1 and actual == 1 else
+                             "False Positive" if pred == 1 and actual == 0 else
+                             "True Negative" if pred == 0 and actual == 0 else
+                             "False Negative" for pred, actual in zip(y_pred, y_test)]
+
+        # Add the prediction result to the DataFrame for visualization
+        df["Prediction Result"] = prediction_result
 
         # Display evaluation metrics
-        st.write("Evaluation Metrics:")
+        st.subheader("Evaluation Metrics:")
         st.write("Accuracy:", accuracy)
         st.write("Precision:", precision)
         st.write("Recall:", recall)
         st.write("F1 Score:", f1)
         st.write("Prediction Time:", end_time - start_time, "seconds")
 
-        # Generate insights and visualizations
-        st.header("Insights and Visualizations")
+        # Display prediction result for each transaction
+        st.subheader("Prediction Results for Each Transaction")
+        st.write(df[["Class", "Prediction Result"]])
 
-        # Plot confusion matrix
-        plot_confusion_matrix(y_test, y_pred.round())
 
-        # Plot precision-recall curve
-        plot_precision_recall_curve(y_test, y_pred)
+        if input_file == "uploaded-file":
+            # Generate insights and visualizations
+            st.header("Insights and Visualizations")
+            # Plot confusion matrix
+            st.pyplot(plot_confusion_matrix(y_test, y_pred))
+            # Plot precision-recall curve
+            st.pyplot(plot_precision_recall_curve(y_test, y_pred))
+            # Plot ROC curve
+            st.pyplot(plot_roc_curve(y_test, y_pred))
 
-        # Plot ROC curve
-        plot_roc_curve(y_test, y_pred)
 
-        # Plot feature importance
-        plot_feature_importance(X_test, model)
-
+def simulate_loading(message, loadTime):
+    # Simulate data loading with a sleep timer
+    with st.spinner(message):
+        time.sleep(loadTime)
 
 def plot_histogram(df):
     plt.figure(figsize=(10, 6))
@@ -196,7 +198,7 @@ def plot_confusion_matrix(y_true, y_pred):
     plt.xlabel('Predicted labels')
     plt.ylabel('True labels')
     plt.title('Confusion Matrix')
-    st.pyplot()
+    return plt
 
 # Function to plot precision-recall curve
 def plot_precision_recall_curve(y_true, y_pred):
@@ -206,7 +208,7 @@ def plot_precision_recall_curve(y_true, y_pred):
     plt.xlabel('Recall')
     plt.ylabel('Precision')
     plt.title('Precision-Recall Curve')
-    st.pyplot()
+    return plt
 
 # Function to plot ROC curve
 def plot_roc_curve(y_true, y_pred):
@@ -221,22 +223,7 @@ def plot_roc_curve(y_true, y_pred):
     plt.ylabel('True Positive Rate')
     plt.title('Receiver Operating Characteristic (ROC) Curve')
     plt.legend(loc='lower right')
-    st.pyplot()
-
-# Function to visualize feature importance
-def plot_feature_importance(X_test, model):
-    # Assuming model has attribute feature_importances_
-    feature_importance = model.feature_importances_
-    feature_names = X_test.columns
-    sorted_idx = np.argsort(feature_importance)
-    plt.figure(figsize=(10, 8))
-    plt.barh(range(len(sorted_idx)), feature_importance[sorted_idx], align='center')
-    plt.yticks(range(len(sorted_idx)), [feature_names[i] for i in sorted_idx])
-    plt.xlabel('Feature Importance')
-    plt.ylabel('Features')
-    plt.title('Feature Importance')
-    st.pyplot()
-
+    return plt
 
 if __name__ == "__main__":
     main()
